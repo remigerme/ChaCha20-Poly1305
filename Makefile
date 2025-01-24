2f3963ac0454a2acf644e7eeed4b3981761eb86d
remi.germe <remi.germe@gmail.com>
2025-01-24 12:23:09 +0100

    quarter-round test

--- Makefile
+++ Makefile
@@ -10,5 +10,11 @@ poly1305-gen: src/utils.c src/poly1305.c src/poly1305_gen.c
 poly1305-check: src/utils.c src/poly1305.c src/poly1305_check.c
 	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $^
 
+tests: quarter-round
+	./quarter-round
+
+quarter-round: src/chacha20.c tests/quarter_round.c
+	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $^
+
 clean:
-	rm -f poly1305-gen poly1305-check
+	rm -f poly1305-gen poly1305-check quarter_round
---
+++ tests/quarter_round.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "chacha20.h"
+
+// See section 2.2.1
+int main() {
+    uint32_t state[16] = {0x879531e0, 0xc5ecf37d, 0x516461b1, 0xc9a62f8a,
+                          0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0x2a5f714c,
+                          0x53372767, 0xb00a5631, 0x974c541a, 0x359e9963,
+                          0x5c971061, 0x3d631689, 0x2098d9d6, 0x91dbd320};
+
+    printf("Initial state :\n");
+    print_state(state);
+
+    quarter_round(state, 2, 7, 8, 13);
+
+    printf("After applying QUARTERROUND(2, 7, 8, 13) :\n");
+    print_state(state);
+
+    uint32_t expected_state[16] = {
+        0x879531e0, 0xc5ecf37d, 0xbdb886dc, 0xc9a62f8a, 0x44c20ef3, 0x3390af7f,
+        0xd9fc690b, 0xcfacafd2, 0xe46bea80, 0xb00a5631, 0x974c541a, 0x359e9963,
+        0x5c971061, 0xccc07c79, 0x2098d9d6, 0x91dbd320};
+
+    int equal = 1;
+    for (int i = 0; i < 16; ++i)
+        equal &= state[i] == expected_state[i];
+
+    return 1 - equal;
+}
